3955b3f9e9b617e4b3bdf8a990cc0af86194c0f0
Abby Jane <225737586+abigailmorton@users.noreply.github.com>
2025-12-08 06:47:59 -0600

    First commit

--- build/html/_sources/index.rst.txt
+++ build/html/_sources/index.rst.txt
@@ -1,20 +1,15 @@
-.. Abby Jane Knowledge Base documentation master file, created by
-   sphinx-quickstart on Mon Dec  8 06:16:12 2025.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+Abby Jane’s Knowledge Base
+=========================
 
-Welcome to Abby Jane Knowledge Base's documentation!
-====================================================
+This is my homegrown knowledge base: notes on infrastructure, Ghost,
+certificates, and the things 3 AM Abby refuses to remember.
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: Sections:
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   infra/index
+   ghost/index
+   certs/index
+   linux/index
+   misc/index
--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Abby Jane Knowledge Base'
+project = 'Abby Jane\'s Knowledge Base'
 copyright = '2025, Abby Jane'
 author = 'Abby Jane'
 release = '1'
@@ -16,6 +16,7 @@ release = '1'
 
 extensions = [
         "myst_parser",
+        "alabaster",
 ]
 
 source_suffix = {
@@ -33,3 +34,42 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_css_files = ["style.css"]
+
+html_theme_options = {
+    "description": "Abby Jane’s homegrown knowledge base: infra, Ghost notes, certs, and what 3 AM Abby keeps forgetting.",
+    "fixed_sidebar": True,
+    "page_width": "960px",
+    "sidebar_width": "240px",
+
+    # If you want your name at the top instead of just the project title
+    "logo_name": True,
+
+    # Nice subtle header link colors
+    "link": "#c2185b",
+    "link_hover": "#7b1fa2",
+
+    # Optional footer text
+    "show_relbars": False,
+    "logo_name": True,
+}
+
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "searchbox.html",
+    ],
+}
+
+html_use_modindex = False
+
+html_theme_options = {
+    "description": "infra, Ghost notes, certs, and what 3 AM Abby Jane keeps forgetting.",
+    "fixed_sidebar": True,
+    "logo_name": True,
+    "show_relbars": False,
+}
+
+html_last_updated_fmt = ""
+html_show_sphinx = False
